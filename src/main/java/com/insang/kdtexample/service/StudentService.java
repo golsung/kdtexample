484aeeefc887336b5930faa55d0dbd75ba83363a
ischung <golsung@naver.com>
2020-01-17 19:44:38 +0900

    Kdt example

---
+++ src/main/java/com/insang/kdtexample/service/StudentService.java
@@ -0,0 +1,72 @@
+package com.insang.kdtexample.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.insang.kdtexample.domain.Grade;
+import com.insang.kdtexample.domain.Student;
+import com.insang.kdtexample.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	public Student addStudentGrade(Student student) {
+		computeGrade(student);
+		Student created = studentRepository.saveAndFlush(student);
+		return created;
+	}
+
+	private void computeGrade(Student student) {
+		double m = student.getMidScore()*0.35;
+		double f = student.getFinalScore()*0.35;
+		double h = student.getHwScore()*0.3;
+		
+		double totalScore = m+f+h;
+		
+		student.setTotalScore(totalScore);
+		if (totalScore>=90.0)
+			student.setGrade(Grade.A);
+		else if (totalScore>=80.0)
+			student.setGrade(Grade.B);
+		else if (totalScore>=70.0)
+			student.setGrade(Grade.C);
+		else if (totalScore>=60.0)
+			student.setGrade(Grade.D);
+		else 
+			student.setGrade(Grade.F);
+		
+	}
+
+	public List<Student> findAll() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
+
+	public Student find(Long id) {
+		Optional<Student> student = studentRepository.findById(id);
+		Student s;
+		if (student.isPresent())
+			s = student.get();
+		else s = null;
+			
+		return s;
+	}
+	
+	public void deleteStudentGrade(Long id) {
+		studentRepository.deleteById(id);
+	}
+
+	public void deleteAll() {
+		studentRepository.deleteAll();
+		
+	}
+}
